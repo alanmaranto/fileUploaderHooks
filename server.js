5acca7ed2944f4626e66f55374aef106e0ed255f
alan <alanarostegui@hotmail.com>
2020-03-28 23:45:02 -0600

    refactor[server]: change console.error

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(fileUpload());
 //Upload endpoint
 app.post('/upload', (req,res) => {
     if (req.files === null) {
-        return res.status(400).json({ msg: 'No ile uploaded'});
+        return res.status(400).json({ msg: 'No file uploaded'});
     }
 
     const file = req.files.file;
 
     file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
         if (err) {
-            console.log(err);
+            console.error(err);
             return res.status(500).send(err);
         }
 
