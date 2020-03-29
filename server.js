b9615f44d6ce8583b50d806a662f69c93dc2d2cb
alan <alanarostegui@hotmail.com>
2020-03-28 21:47:23 -0600

    feature(server): file endpoint

--- server.js
+++ server.js
@@ -6,4 +6,22 @@ const app = express();
 //middleware
 app.use(fileUpload());
 
+//Upload endpoint
+app.post('/upload', (req,res) => {
+    if (req.files === null) {
+        return res.status(400).json({ msg: 'No ile uploaded'});
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+    });
+})
+
 app.listen(5000, () => console.log('Server Started...'));
