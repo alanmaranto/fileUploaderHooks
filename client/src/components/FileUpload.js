cda1125c3c1d3bdd3d2e66ae02293e50d456a0a9
alan <alanarostegui@hotmail.com>
2020-03-29 01:27:24 -0600

    feature[client](message): add message alert

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Message from "./Message";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
 
   const onChange = e => {
     setFile(e.target.files[0]);
@@ -27,17 +29,20 @@ const FileUpload = () => {
       const { fileName, filePath } = result.data;
 
       setUploadedFile({ fileName, filePath });
+
+      setMessage("File Uploaded");
     } catch (err) {
       if (err.response.status === 500) {
-        console.log("There was a problem with the server");
+        setMessage("There was a problem with the server");
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
