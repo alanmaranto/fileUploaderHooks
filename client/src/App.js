98e454a2d8d5b467ef587fab8c2511bc40b5b4ca
alan <alanarostegui@hotmail.com>
2020-03-28 22:39:52 -0600

    feature[client](app): set up title with fontawesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact } from '@fortawesome/free-brands-svg-icons';
 
 const App = () => {
-  return ( 
-    <div className="App">
-      App
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+        <FontAwesomeIcon icon={faReact}/>
+        <i className="fab fa-react"/> React File Upload
+      </h4>
     </div>
-   );
-}
- 
+  );
+};
+
 export default App;
